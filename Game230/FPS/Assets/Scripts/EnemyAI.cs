42017c561fb1c32ac82d917d24ae278a72cc6036
Dawson Mix <dmix18@apu.edu>
2021-03-30 14:30:02 -0700

    Molded the terrain
    created new level and created the landscape still no textures

--- Game230/FPS/Assets/Scripts/EnemyAI.cs
+++ Game230/FPS/Assets/Scripts/EnemyAI.cs
@@ -6,6 +6,9 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5.0f;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false; 
 
     NavMeshAgent nMa; 
 
@@ -18,6 +21,45 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget >= nMa.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        if (distanceToTarget <= nMa.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget()
+    {
         nMa.SetDestination(target.position);
     }
+
+    private void AttackTarget()
+    {
+        //ttemp
+        print(name + "is attacking " + target.name);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(255f, 0f, 0f);
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+
 }
---
+++ Game230/FPS/Assets/Scripts/Weapon.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera fPCamera;
+    [SerializeField] float range = 100.0f;
+    [SerializeField] float damage = 30.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(fPCamera.transform.position, fPCamera.transform.forward, out hit, range))
+        {
+            print("Raycast has hit: " + hit.transform.name);
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null)
+            {
+                return;
+            }
+            target.TakeDamage(damage);
+        }
+        else
+        {
+            return;        
+        }
+    }
+}
